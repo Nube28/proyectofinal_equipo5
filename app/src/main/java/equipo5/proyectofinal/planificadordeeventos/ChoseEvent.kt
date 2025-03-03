956634341fe46909fd8f4d231862275a573e5985
Nube28 <159978169+Nube28@users.noreply.github.com>
2025-03-03 13:15:30 -0700

    EventDetail on progress

--- app/src/main/java/equipo5/proyectofinal/planificadordeeventos/ChoseEvent.kt
+++ app/src/main/java/equipo5/proyectofinal/planificadordeeventos/ChoseEvent.kt
@@ -1,14 +1,25 @@
 package equipo5.proyectofinal.planificadordeeventos
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ChoseEvent : AppCompatActivity() {
+
+    var adapter: EventOverviewAdapter? = null
+    var eventsOverview = ArrayList<EventOverview>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,20 +30,61 @@ class ChoseEvent : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var listView: ListView = findViewById(R.id.list_view) as ListView
+
+        cargarEventos()
+        adapter = EventOverviewAdapter(this, eventsOverview)
+        listView.adapter = adapter
+
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        val headerProfileFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as? HeaderProfile
-
-        headerProfileFragment?.view?.let { fragmentView ->
-            val btnProfile: ImageView = fragmentView.findViewById(R.id.btn_profile)
-
-            btnProfile.setOnClickListener {
-                val intent = Intent(this, AccountConfig::class.java)
-                startActivity(intent)
-            }
-        }
+    fun cargarEventos(){
+        eventsOverview.add(EventOverview(R.drawable.lain, "Fiesta de bell", "10000", "Fiesta"))
+        eventsOverview.add(EventOverview(R.drawable.lain, "Boda de bell", "100000", "Boda"))
+        eventsOverview.add(EventOverview(R.drawable.lain, "Boda de gomez", "1000", "Boda"))
     }
+}
+
+class EventOverviewAdapter: BaseAdapter {
+    var eventsOverview = ArrayList<EventOverview>()
+    var context: Context? = null
+
+    constructor(context: Context, eventsOverview: ArrayList<EventOverview>){
+        this.context = context
+        this.eventsOverview = eventsOverview
+    }
+
+    override fun getCount(): Int {
+        return eventsOverview.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return eventsOverview[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var eventOverview = eventsOverview[position]
+        var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var view = inflator.inflate(R.layout.event_overview, null)
+
+        val event_iv_overview = view.findViewById(R.id.event_iv_overview) as ImageView
+        val event_name_overview = view.findViewById(R.id.event_name_overview) as TextView
+        val event_budget_overview = view.findViewById(R.id.event_budget_overview) as TextView
+        val event_category_overview = view.findViewById(R.id.event_category_overview) as TextView
+
+        event_iv_overview.setImageResource(eventOverview.event_iv_overview)
+        event_name_overview.setText(eventOverview.event_name_overview)
+        event_budget_overview.setText(eventOverview.event_budget_overview)
+        event_category_overview.setText(eventOverview.event_category_overview)
+
+
+
+        return view
+    }
+
 }
