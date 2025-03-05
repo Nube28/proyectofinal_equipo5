e188fd8f89c9f7ea634d134fc4a1d45824ca644c
Nube28 <alejandro.gomez247313@potros.itson.edu.mx>
2025-03-04 21:33:41 -0600

    New Correct Classes

--- app/src/main/java/equipo5/proyectofinal/planificadordeeventos/AddSubtask.kt
+++ app/src/main/java/equipo5/proyectofinal/planificadordeeventos/AddSubtask.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class AddSupplier : AppCompatActivity() {
+class AddSubtask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_supplier)
+        setContentView(R.layout.activity_add_subtask)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
